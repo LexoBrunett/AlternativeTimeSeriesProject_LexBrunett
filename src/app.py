b8c3cde773ab685b0e3da3aa7561c68c8c1652ef
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-10 20:00:10 +0000

    model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,114 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+import pandas as pd
+
+total_data = pd.read_csv("sales.csv")
+
+total_data.head()
+
+
+total_data["date"] = pd.to_datetime(total_data.date)
+total_data = total_data.set_index("date")
+ts_total = total_data["sales"]
+ts_total.head()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+sns.lineplot(data = ts_total)
+
+plt.tight_layout()
+
+plt.show()
+
+from statsmodels.tsa.stattools import adfuller
+
+def test_stationarity(timeseries):
+    print("Dickey-Fuller test results:")
+    dftest = adfuller(timeseries, autolag = "AIC")
+    dfoutput = pd.Series(dftest[0:4], index = ["Test Statistic", "p-value", "#Lags Used", "Number of Observations Used"])
+    for key,value in dftest[4].items():
+        dfoutput["Critical Value (%s)"%key] = value
+    return dfoutput
+
+test_stationarity(ts_total)
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+decomposition = seasonal_decompose(ts_total)
+decomposition
+
+trend = decomposition.trend
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+sns.lineplot(data = ts_total)
+sns.lineplot(data = trend)
+
+plt.tight_layout()
+
+plt.show()
+
+seasonal = decomposition.seasonal
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+sns.lineplot(data = ts_total)
+sns.lineplot(data = seasonal)
+
+plt.tight_layout()
+
+plt.show()
+
+from statsmodels.tsa.stattools import adfuller
+
+def test_stationarity(timeseries):
+    print("Dickey-Fuller test results:")
+    dftest = adfuller(timeseries, autolag = "AIC")
+    dfoutput = pd.Series(dftest[0:4], index = ["Test Statistic", "p-value", "#Lags Used", "Number of Observations Used"])
+    for key,value in dftest[4].items():
+        dfoutput["Critical Value (%s)"%key] = value
+    return dfoutput
+
+test_stationarity(ts_total)
+
+residual = decomposition.resid
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+sns.lineplot(data = ts_total)
+sns.lineplot(data = residual)
+
+plt.tight_layout()
+
+plt.show()
+
+from statsmodels.graphics.tsaplots import plot_acf
+
+plot_acf(ts_total)
+
+plt.tight_layout()
+
+plt.show()
+
+from pmdarima import auto_arima
+
+model = auto_arima(ts_total, seasonal = False, trace = True, m = 7)
+
+forecast = model.predict(60)
+forecast
+
+import matplotlib.pyplot as plt
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+sns.lineplot(data = ts_total)
+sns.lineplot(data = forecast, c = "green")
+
+plt.tight_layout()
+
+plt.show()
+
